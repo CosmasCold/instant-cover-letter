b475e72639a5cc7e08c1e54b533faae8f4a602e5
Gabriel <coldcosmas@gmail.com>
2026-05-04 10:54:55 -0400

    Add Impact tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +31,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <meta name="impact-site-verification" content="d7efa8ff-fff5-4824-93e1-9a38fd81395f" />
+      </head>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script id="impact-tracker" strategy="afterInteractive">
+          {`(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7265734-1451-4faf-bbbc-d52d4dca90731.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`}
+        </Script>
+      </body>
     </html>
   );
 }
