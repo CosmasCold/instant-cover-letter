1f0c5a4c73f6768d476f9ede975d15c8d6900aa4
Gabriel <coldcosmas@gmail.com>
2026-05-04 15:29:28 -0400

    Add Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+         <SpeedInsights />
         <Script id="impact-tracker" strategy="afterInteractive">
           {`(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7265734-1451-4faf-bbbc-d52d4dca90731.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`}
         </Script>
